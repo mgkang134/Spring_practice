08d2cccede57727d9b0adf39fd36e882a2ee9297
mgkang134 <mgkang134@gmail.com>
2020-08-06 17:46:32 +0900

    회원 등록 기능

--- src/main/java/com/example/demo/controller/MemberController.java
+++ src/main/java/com/example/demo/controller/MemberController.java
@@ -2,7 +2,10 @@ package com.example.demo.controller;
 
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PostMapping;
 
+import com.example.demo.domain.Member;
 import com.example.demo.service.MemberService;
 
 @Controller
@@ -13,6 +16,22 @@ public class MemberController {
 	@Autowired
 	public MemberController(MemberService memberService) {
 		this.memberService = memberService;
+	}
+	
+	@GetMapping("/members/new")
+	public String createForm() {
+		return "members/createMemberForm";
+	}
+	
+	@PostMapping("/members/new")
+	public String create(MemberForm form) {
+		Member member = new Member();
+		member.setName(form.getName());
+		
+		memberService.join(member);
+		
+		return "redirect:/";
+		
 	}
 	
 	
