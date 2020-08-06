72d127d6edf5a8f073677471a53e772b76c5ce09
mgkang134 <mgkang134@gmail.com>
2020-08-06 16:19:38 +0900

    컴포넌트 스캔, 의존관계 설정(자동)

--- src/main/java/com/example/demo/service/MemberService.java
+++ src/main/java/com/example/demo/service/MemberService.java
@@ -3,10 +3,14 @@ package com.example.demo.service;
 import java.util.List;
 import java.util.Optional;
 
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Service;
+
 import com.example.demo.domain.Member;
 import com.example.demo.repository.MemberRepository;
 import com.example.demo.repository.MemoryMemberRepository;
 
+@Service
 public class MemberService {
 
 	private final MemberRepository memberRepository;
