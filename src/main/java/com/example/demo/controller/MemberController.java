8036f00602b91a912dae7e19a2feba5a022d6eb0
mgkang134 <mgkang134@gmail.com>
2020-08-06 18:19:35 +0900

    회원 조회 기능

--- src/main/java/com/example/demo/controller/MemberController.java
+++ src/main/java/com/example/demo/controller/MemberController.java
@@ -1,7 +1,10 @@
 package com.example.demo.controller;
 
+import java.util.List;
+
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Controller;
+import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PostMapping;
 
@@ -23,6 +26,13 @@ public class MemberController {
 		return "members/createMemberForm";
 	}
 	
+	@GetMapping("/members")
+	public String list(Model model) {
+		List<Member> members = memberService.findMembers();
+		model.addAttribute("members", members);
+		return "members/memberList";
+	}
+	
 	@PostMapping("/members/new")
 	public String create(MemberForm form) {
 		Member member = new Member();
@@ -31,7 +41,6 @@ public class MemberController {
 		memberService.join(member);
 		
 		return "redirect:/";
-		
 	}
 	
 	
