9a5e82d8ea50815fdb1a694feba2a9c30cf4ed41
mgkang134 <mgkang134@gmail.com>
2020-08-06 15:48:55 +0900

    Service 및 Test코드 작성

---
+++ src/main/java/com/example/demo/service/MemberService.java
@@ -0,0 +1,41 @@
+package com.example.demo.service;
+
+import java.util.List;
+import java.util.Optional;
+
+import com.example.demo.domain.Member;
+import com.example.demo.repository.MemberRepository;
+import com.example.demo.repository.MemoryMemberRepository;
+
+public class MemberService {
+
+	private final MemberRepository memberRepository;
+
+	public MemberService(MemberRepository memberRepository) {
+		this.memberRepository = memberRepository;
+	}
+
+	public long join(Member member) {
+		//같은 이름이 있는 중복 회원 X
+		validateDuplicateMember(member);
+		 memberRepository.save(member);
+		 return member.getId();
+	}
+
+	private void validateDuplicateMember(Member member) {
+		memberRepository.findByName(member.getName())
+			.ifPresent(m->{
+				throw new IllegalStateException("이미 존재하는 회원입니다.");
+			});
+	}
+	
+	//전체 회원 조회
+	public List<Member> findMembers(){
+		return memberRepository.findAll();
+	}
+	
+	//회원 한 명 조회
+	public Optional<Member> findOne(long memberId){
+		return memberRepository.findById(memberId);
+	}
+}
