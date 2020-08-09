01fb47d288851c944de3c76c2118b3d1e19a2131
mgkang134 <mgkang134@gmail.com>
2020-08-09 15:19:56 +0900

    Data-Jpa

--- src/main/java/com/example/demo/SpringConfig.java
+++ src/main/java/com/example/demo/SpringConfig.java
@@ -1,36 +1,29 @@
 package com.example.demo;
 
-import javax.persistence.EntityManager;
-import javax.sql.DataSource;
-
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
-
-import com.example.demo.repository.JdbcMemberRepository;
-import com.example.demo.repository.JdbcTemplateMemberRepository;
-import com.example.demo.repository.JpaMemberRepository;
 import com.example.demo.repository.MemberRepository;
-import com.example.demo.repository.MemoryMemberRepository;
 import com.example.demo.service.MemberService;
 
 @Configuration
 public class SpringConfig {
 	
-	private EntityManager em;
-	
+	private final MemberRepository memberRepository;
+
 	@Autowired
-	public SpringConfig(EntityManager em) {
-		this.em = em;
+	public SpringConfig(MemberRepository memberRepository) {
+		this.memberRepository = memberRepository;
 	}
 
 	@Bean
 	public MemberService memberService() {
-		return new MemberService(memberRepository());
+		return new MemberService(memberRepository);
 	}
 	
-	@Bean
-	public MemberRepository memberRepository() {
-		return new JpaMemberRepository(em);
-	}
+//	@Bean
+//	public MemberRepository memberRepository() {
+////		return new JpaMemberRepository(em);
+//		
+//	}
 }
